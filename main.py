2f6b849405cb291277d82532ef5183afd6ecb32d
404zhan <onedirection123124@gmail.com>
2025-07-07 18:45:29 +0530

    added the asteroid field logic, and logic for movement of the spaceship

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
+from asteroid import Asteroid
+from asteroidfield import *
 from constants import *
 from player import *
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 def main():
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
@@ -11,14 +16,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60)/1000
         screen.fill("black")
         player.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
-        dt = clock.tick(60)/1000
+        
 
 if __name__ == "__main__":
     main()
